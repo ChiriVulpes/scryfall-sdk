b3c79ce64f83047f4eceaa74f3431340cef3332d
Silvio Schmidt <sili3011@web.de>
2022-02-20 19:32:46 +0100

    added additional prices fields

--- src/api/Cards.ts
+++ src/api/Cards.ts
@@ -143,10 +143,12 @@ export interface ImageUris {
 }
 
 export interface Prices {
-	usd?: string | null;
-	usd_foil?: string | null;
-	eur?: string | null;
-	tix?: string | null;
+    usd?: string | null;
+    usd_foil?: string | null;
+    usd_etched?: string | null;
+    eur?: string | null;
+    eur_foil?: string | null;
+    tix?: string | null;
 }
 
 export interface PurchaseUris {
